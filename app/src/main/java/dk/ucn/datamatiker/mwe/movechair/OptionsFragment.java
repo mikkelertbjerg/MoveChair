e67994bd41aa948eae77cb80b238a1d373c23b7f
Mikkel Wexøe Ertbjerg <1065500@ucn.dk>
2018-11-07 19:39:19 +0100

    Added logout logic Changed launcher activity

--- app/src/main/java/dk/ucn/datamatiker/mwe/movechair/OptionsFragment.java
+++ app/src/main/java/dk/ucn/datamatiker/mwe/movechair/OptionsFragment.java
@@ -1,6 +1,7 @@
 package dk.ucn.datamatiker.mwe.movechair;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,8 +10,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class OptionsFragment extends Fragment {
+public class OptionsFragment extends Fragment implements View.OnClickListener {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -25,5 +27,14 @@ public class OptionsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         //This makes you able to change toolbar title
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Options");
+        Button signOutButton = (Button) view.findViewById(R.id.sign_out_button);
+        signOutButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(getActivity(), LoginActivity.class);
+        getActivity().finish(); //Kill the current activity
+        startActivity(i);
     }
 }
