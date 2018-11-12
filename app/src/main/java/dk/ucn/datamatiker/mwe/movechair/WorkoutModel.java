bce4e36046d48123221e1998bdb46c190707653f
Mikkel Wexøe Ertbjerg <1065500@ucn.dk>
2018-11-12 22:58:38 +0100

    Added dummy data testing and some lovely bugs

--- app/src/main/java/dk/ucn/datamatiker/mwe/movechair/WorkoutModel.java
+++ app/src/main/java/dk/ucn/datamatiker/mwe/movechair/WorkoutModel.java
@@ -5,23 +5,23 @@ import java.util.ArrayList;
 
 public class WorkoutModel extends ActivityModel{
 
-    private ArrayList<ExerciseModel> exercises;
+    private ArrayList<ActivityModel> exercises;
 
-    public WorkoutModel(String name, String description, int id, ArrayList<ExerciseModel> exercises) {
+    public WorkoutModel(String name, String description, int id, ArrayList<ActivityModel> exercises) {
         super(name, description, id);
         this.exercises = exercises;
     }
 
     public WorkoutModel(String name, String description, int id) {
         super(name, description, id);
-        this.exercises = new ArrayList<ExerciseModel>();
+        this.exercises = new ArrayList<ActivityModel>();
     }
 
-    public ArrayList<ExerciseModel> getExercises() {
+    public ArrayList<ActivityModel> getExercises() {
         return exercises;
     }
 
-    public void setExercises(ArrayList<ExerciseModel> exercises) {
+    public void setExercises(ArrayList<ActivityModel> exercises) {
         this.exercises = exercises;
     }
 }
