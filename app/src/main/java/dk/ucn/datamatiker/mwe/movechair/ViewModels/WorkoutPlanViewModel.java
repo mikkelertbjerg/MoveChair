0330c02300f27819555cfb94e209c8db14547aa8
Mikkel Wexøe Ertbjerg <1065500@ucn.dk>
2018-11-19 10:37:22 +0100

    Deleteted some unused refs.

--- app/src/main/java/dk/ucn/datamatiker/mwe/movechair/ViewModels/WorkoutPlanViewModel.java
+++ app/src/main/java/dk/ucn/datamatiker/mwe/movechair/ViewModels/WorkoutPlanViewModel.java
@@ -1,26 +1,9 @@
 package dk.ucn.datamatiker.mwe.movechair.ViewModels;
 
-import android.app.Application;
-import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.ViewModel;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
 import java.util.List;
-
-import dk.ucn.datamatiker.mwe.movechair.ActivityAdapter;
 import dk.ucn.datamatiker.mwe.movechair.Models.ActivityModel;
 import dk.ucn.datamatiker.mwe.movechair.Models.WorkoutPlanModel;
-import dk.ucn.datamatiker.mwe.movechair.R;
 import dk.ucn.datamatiker.mwe.movechair.Test.DummyData;
 
 
