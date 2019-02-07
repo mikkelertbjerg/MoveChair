b0df82ff1ecbc40dc8020dd6e2dea5c1de0f504e
Whatten <morten.thammawat@gmail.com>
2019-02-07 10:08:31 +0100

    Added googlefit subclass as an activity

--- app/src/main/java/dk/ucn/datamatiker/mwe/movechair/LoginActivity.java
+++ app/src/main/java/dk/ucn/datamatiker/mwe/movechair/LoginActivity.java
@@ -1,13 +1,36 @@
 package dk.ucn.datamatiker.mwe.movechair;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.result.DataReadResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 import dk.ucn.datamatiker.mwe.movechair.Fragments.LoginFragment;
+
+import static com.loopj.android.http.AsyncHttpClient.LOG_TAG;
+
 @RequiresApi(api = Build.VERSION_CODES.P)
 public class LoginActivity extends AppCompatActivity  {
 
@@ -36,5 +59,6 @@ public class LoginActivity extends AppCompatActivity  {
                     .commit();
         }
     }
+
 }
 
